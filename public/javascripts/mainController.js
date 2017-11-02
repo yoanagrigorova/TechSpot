e42a28b0492c97f7f70034609ecba337022508ca
yoanagrigorova <31156123+yoanagrigorova@users.noreply.github.com>
2017-11-02 12:36:16 +0200

    Favorite collection and events on buy and add to favorite buttons

--- public/javascripts/mainController.js
+++ public/javascripts/mainController.js
@@ -40,4 +40,15 @@ angular.module("MainCtrl", ['ngCookies'])
             $scope.getCurrentUser();
 
         }
+
+        $scope.addToFavorites = function(item) {
+            $scope.getCurrentUser();
+            if (!$rootScope.userInSess.favorites.some(x => x.title == item.title)) {
+                $rootScope.userInSess.favorites.push(item);
+            } else {
+                var product = $rootScope.userInSess.favorites.find(x => x.title == item.title);
+                product.quantity++;
+            }
+            localStorage.setItem('currentUser', JSON.stringify($rootScope.userInSess));
+        }
     })
--- public/javascripts/searchCtr.js
+++ public/javascripts/searchCtr.js
@@ -4,14 +4,29 @@ angular.module('searchApp', ['ngAnimate'])
         $scope.categories = [
             { name: 'Телефони', url: '/api/phones' },
             { name: 'Телевизори', url: '/api/tvs' },
-            { name: 'Компютри', url: '/api/computers' }
+            { name: 'Компютри', url: '/api/computers' },
+            { name: "Готварски печки", url: "/homeappliances/api/ovens" },
+            { name: "Хладилници", url: "/homeappliances/api/fridges" },
+            { name: "Перални", url: "/homeappliances/api/washing-machines" },
+            { name: "Микровулнови печки", url: "/smallappliances/api/microwave-ovens" },
+            { name: "Прахосмукачки", url: "/smallappliances/api/vacuumCleaners" },
+            { name: "Климатици", url: "/api/conditioners" }
         ];
 
 
         $scope.changeValue = function(item) {
             console.log(item);
+            var url = "";
+            if (item.url.indexOf("homeappliances") !== -1) {
+                url = item.url.substr(0, 15);
+                item.url = item.url.substr(15);
+            }
+            if (item.url.indexOf("smallappliances") !== -1) {
+                url = item.url.substr(0, 16);
+                item.url = item.url.substr(16);
+            }
             $http.get(item.url).then(function(response) {
-                $scope.category = item.url.substr(5);
+                $scope.category = url + "" + item.url.substr(4);
                 $scope.searchResults = response.data;
             })
         }
